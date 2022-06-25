0eb8849207e6b6e342fb90420f6706cd96cfce8e
hundun <wwj.hddjj@gmail.com>
2022-06-25 14:11:15 +0800

    use petpet 3.2

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 - 以Command的方式提供服务，不需自己实现指令匹配
 - 通过[SingletonDocumentRepository工具](https://github.com/hundun000/mirai-fleet-framework/blob/main/framework-helper/src/main/java/hundun/miraifleet/framework/helper/repository/SingletonDocumentRepository.java)读取（和写默认值）配置文件。而不是通过JavaAutoSaveConfig读取配置
+- 可通过console指令测试制图结果，保存在本地
 
 ## 功能说明
 
@@ -13,6 +14,7 @@
 ```
 ◆ /制图 选择模板 <模板key> <模板头像位置替换的对象> <(可选)模板文本替换的参数>
   /制图 选择模板 <模板key> <(可选)模板文本替换的参数>
+  /制图 测试模板 <模板key> <(可选)模板文本替换的参数>
   /制图 摸 <摸的对象>
 ```
 
@@ -20,7 +22,15 @@
 ![image](docs/摸.png)  
 ![image](docs/kiss.png)
 
-模板里的`data.json`规则，见[Petpet项目](https://github.com/Dituon/petpet)。
+模板位于`data\hundun.mirai.simplepetpet\templates`，其中每一个模板来自[同版本的Petpet项目](https://github.com/Dituon/petpet/tree/3.2)，可从那边复制补充到本项目。
+
+运行`PluginTest`后，即可使用`/制图 测试模板 ……`指令，例如：
+> /制图 测试模板 petpet 
+>  
+> 保存成功：petpet-XXXXXXXXXX.gif  
+
+此处文件保存于`data\hundun.mirai.simplepetpet\save`：  
+![image](docs/petpet-save.jpg)  
 
 ## 其他
 
--- src/main/java/hundun/mirai/simplepetpet/PetpetCommand.java
+++ src/main/java/hundun/mirai/simplepetpet/PetpetCommand.java
@@ -2,19 +2,25 @@ package hundun.mirai.simplepetpet;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
 
 import hundun.miraifleet.framework.helper.repository.SingletonDocumentRepository;
 import kotlin.Pair;
 import net.mamoe.mirai.console.command.CommandSender;
 import net.mamoe.mirai.console.command.CompositeCommand;
+import net.mamoe.mirai.console.command.ConsoleCommandSender;
 import net.mamoe.mirai.console.command.descriptor.CommandArgumentContext;
 import net.mamoe.mirai.console.plugin.jvm.JvmPlugin;
 import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.utils.ExternalResource;
+import xmmt.dituon.share.AvatarExtraData;
 import xmmt.dituon.share.BasePetService;
 import xmmt.dituon.share.BaseServiceConfig;
 import xmmt.dituon.share.ImageSynthesis;
@@ -25,6 +31,8 @@ public class PetpetCommand extends CompositeCommand {
     private final BasePetService petService;
     private final JvmPlugin plugin;
     private final SingletonDocumentRepository<BaseServiceConfig> repository;
+    private BufferedImage defaultAvatar;
+    private File saveFolder;
     
     public PetpetCommand(JvmPlugin plugin) {
         super(plugin, "制图", new String[]{}, "我是PetpetCommand", plugin.getParentPermission(), CommandArgumentContext.EMPTY);
@@ -37,6 +45,7 @@ public class PetpetCommand extends CompositeCommand {
                 () -> new BaseServiceConfig()
                 );
         initPetService(plugin);
+        initSelf();
     }
     
     /**
@@ -49,47 +58,90 @@ public class PetpetCommand extends CompositeCommand {
 
         petService.readBaseServiceConfig(config);
         
-        File petDataFolder = plugin.getDataFolder();
+        File petDataFolder = plugin.resolveDataFile("templates");
         petService.readData(petDataFolder);
+    }
+    
+    private void initSelf() {
+        try {
+            defaultAvatar = ImageIO.read(plugin.resolveDataFile("defaultAvatar.png"));
+        } catch (IOException e) {
+            plugin.getLogger().error(e);
+        }
+        
+        saveFolder = plugin.resolveDataFile("save");
+        if (!saveFolder.exists()) {
+            saveFolder.mkdirs();
+        }
     }
     
     /**
      * 固定使用模板petpet
      */
     @SubCommand("摸")
-    public void petpet(CommandSender sender, 
+    public void petpetCommand(CommandSender sender, 
             @Name("摸的对象") User target) {
-        useTemplateGeneral(sender, "petpet", target, (String[])null);
+        useTemplateAndSend(sender, "petpet", target, (String[])null);
     }
     
     /**
-     * 由用户选择模板petpet
+     * 仅限Console触发，使用defaultAvatar制图，并保存结果到本地
+     */
+    @SubCommand("测试模板")
+    public void useTemplateAndSaveCommand(ConsoleCommandSender sender, 
+            @Name("模板key") String petkey, 
+            @Name("(可选)模板文本替换的参数") String... petReplaceArgs) {
+        useTemplateAndSave(sender, petkey, null, petReplaceArgs);
+    }
+    
+    /**
+     * 由用户选择模板
      */
     @SubCommand("选择模板")
-    public void useTemplateWithTarget(CommandSender sender, 
+    public void useTemplateWithTargetCommand(CommandSender sender, 
             @Name("模板key") String petkey, 
             @Name("模板头像位置替换的对象") User target, 
             @Name("(可选)模板文本替换的参数") String... petReplaceArgs) {
-        useTemplateGeneral(sender, petkey, target, petReplaceArgs);
+        useTemplateAndSend(sender, petkey, target, petReplaceArgs);
     }
     
     /**
-     * 由用户选择模板petpet
+     * 由用户选择模板
      */
     @SubCommand("选择模板")
-    public void useTemplateWithoutTarget(CommandSender sender, 
+    public void useTemplateWithoutTargetCommand(CommandSender sender, 
             @Name("模板key") String petkey, 
             @Name("(可选)模板文本替换的参数") String... petReplaceArgs) {
-        useTemplateGeneral(sender, petkey, null, petReplaceArgs);
+        useTemplateAndSend(sender, petkey, null, petReplaceArgs);
     }
     
-    private void useTemplateGeneral(CommandSender sender, String petkey, User target, String... petReplaceArgs) {
-        // 准备制图参数
+    private void useTemplateAndSave(CommandSender sender, String petkey, User target, String... petReplaceArgs) {
+        BufferedImage fromAvatarImage = defaultAvatar;
+        BufferedImage toAvatarImage = defaultAvatar;
+        
+        Pair<InputStream, String> resultPair = useTemplate(fromAvatarImage, toAvatarImage, petkey, petReplaceArgs);  
+        // 使用制图结果
+        if (resultPair != null) {
+            String saveName = petkey + "-" + System.currentTimeMillis() + "." + resultPair.getSecond();
+            File saveFile = plugin.resolveDataFile(saveFolder.getAbsolutePath() + File.separator + saveName);
+            try {
+                copyInputStreamToFile(resultPair.getFirst(), saveFile);
+                sender.sendMessage("保存成功：" + saveName);
+            } catch (IOException e) {
+                plugin.getLogger().error(e);
+                sender.sendMessage("保存失败");
+            }
+        } else {
+            plugin.getLogger().warning("未得到petService resultPair");  
+            sender.sendMessage("未得到制图结果");
+        }
+    }
+    
+    private void useTemplateAndSend(CommandSender sender, String petkey, User target, String... petReplaceArgs) {
         BufferedImage fromAvatarImage = sender.getUser() != null ? ImageSynthesis.getAvatarImage(sender.getUser().getAvatarUrl()) : null;
         BufferedImage toAvatarImage = ImageSynthesis.getAvatarImage(target.getAvatarUrl());
-        TextExtraData textExtraData = petReplaceArgs != null ? new TextExtraData("", "", "", Arrays.asList(petReplaceArgs)) : null;
-        // 制图
-        Pair<InputStream, String> resultPair = petService.generateImage(fromAvatarImage, toAvatarImage, petkey, textExtraData, null);
+        
+        Pair<InputStream, String> resultPair = useTemplate(fromAvatarImage, toAvatarImage, petkey, petReplaceArgs);
         // 使用制图结果
         if (resultPair != null) {
             try (ExternalResource externalResource = ExternalResource.create(resultPair.getFirst())) {
@@ -108,6 +160,29 @@ public class PetpetCommand extends CompositeCommand {
         } else {
             plugin.getLogger().warning("未得到petService resultPair");  
             sender.sendMessage("未得到制图结果");
+        }
+    }
+    
+    private Pair<InputStream, String> useTemplate(BufferedImage fromAvatarImage, BufferedImage toAvatarImage, String petkey, String... petReplaceArgs) {
+        // 准备制图参数
+        TextExtraData textExtraData = petReplaceArgs != null ? new TextExtraData("", "", "", Arrays.asList(petReplaceArgs)) : null;
+        AvatarExtraData avatarExtraData = new AvatarExtraData(fromAvatarImage, toAvatarImage, null, null);
+        // 制图
+        Pair<InputStream, String> resultPair = petService.generateImage(petkey, avatarExtraData, textExtraData, null);
+        return resultPair;
+    }
+    
+    private void copyInputStreamToFile(InputStream inputStream, File file)
+            throws IOException {
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[8192];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
